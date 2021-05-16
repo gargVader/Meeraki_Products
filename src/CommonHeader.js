8f125d50d0e3994bde4ac9e1dbe7b7bc5224a807
gargVader <girishgargcool@gmail.com>
2021-05-17 01:08:18 +0530

    common  header added, team, contact added

--- src/CommonHeader.js
+++ src/CommonHeader.js
@@ -5,7 +5,142 @@ function CommonHeader() {
 
         <div>
 
-            <h1>Common header</h1>
+            {/* Hero Section start*/}
+            <section id="hero" class="hero d-flex align-items-center">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-6 d-flex flex-column justify-content-center">
+
+                            <h1 data-aos="fade-up">WE ARE HERE FOR YOU</h1>
+                            <h2 data-aos="fade-up" data-aos-delay="400">With innumerable products, an in-house logistics team, and
+                            distribution locations across the India we are the preferred source for raw material solutions. We
+            seamlessly connect our customers and suppliers in winning relationships - globally and domestically.</h2>
+                            <div data-aos="fade-up" data-aos-delay="600">
+                                <div class="text-center text-lg-start">
+                                    <a href="#about"
+                                        class="btn-get-started scrollto d-inline-flex align-items-center justify-content-center align-self-center">
+                                        <span>Know More</span>
+                                        <i class="bi bi-arrow-right"></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 hero-img" data-aos="zoom-out" data-aos-delay="200">
+                            <img src="assets/img/hero-img-2.png" class="img-fluid-custom" alt="" />
+
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+            {/* Hero section end */}
+
+            {/* About section start */}
+            <section id="about" class="about">
+
+                <div class="container" data-aos="fade-up">
+                    <div class="row gx-0">
+
+                        <div class="col-lg-6 d-flex flex-column justify-content-center" data-aos="fade-up" data-aos-delay="200">
+                            <div class="content">
+                                <h3>HOW WE STAND OUT</h3>
+                                <h2>Customer Service Oriented Company</h2>
+                                <p>
+                                    Meeraki is an industry leading chemical distributor with capabilities that ensure you are always stocked
+                                    with the chemicals and
+                                    API’s(Active Pharmaceutical Ingredients) you need. We understand the importance of timely shipping and
+                                    have built our business
+                                    around reliable services. From start to finish, our customers know that they can trust our team to
+                                    deliver only the highest quality
+                                    customer service— and always on time.
+                                </p>
+
+                                <p>
+                                    M/s Meeraki was established to fill the gap that existed in customer service in the chemical
+                                    distribution industry, and
+                                    to help the manufacturers. By being able to have a robust customer service team so that they can also
+                                    make the most of
+                                    this opportunity. M/s Meeraki is redefining the distributor experience by serving customers across the
+                                    globe, using a
+                                    sophisticated supply chain and distribution model that helps manufacturers increase both their profit
+                                    margins and market
+                                    share.
+                                </p>
+
+                                <div class="text-center text-lg-start">
+                                    <a href="#values"
+                                        class="btn-read-more d-inline-flex align-items-center justify-content-center align-self-center">
+                                        <span>Read More</span>
+                                        <i class="bi bi-arrow-right"></i>
+                                    </a>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-6 d-flex align-items-center" data-aos="zoom-out" data-aos-delay="200">
+                            <img src="assets/img/about.jpg" class="img-fluid" alt="" />
+                        </div>
+
+                    </div>
+                </div>
+
+            </section>
+            {/* About section end */}
+
+            {/* Values section start */}
+
+            <section id="values" class="values">
+
+                <div class="container" data-aos="fade-up">
+                    <header class="section-header">
+                        <p>Our Values</p>
+                    </header>
+
+                    <div class="row">
+
+                        <div class="col-lg-4">
+                            <div class="box" data-aos="fade-up" data-aos-delay="200">
+                                <img src="assets/img/Honesty-03.png" class="img-fluid" alt="" />
+                                <h3>Transparency & Honesty</h3>
+                                <p>We firmly believe that the success of our company is built on a foundation
+                                of transparency, trust, and integrity. The strength and diversity of our team
+                                is the most important reason for our success.We currently serve customers in
+                                India and offer overseas manufacturers an entry point to the Indian. We’re
+                                not just a distributor; we are your ideal supply chain partner, helping you
+                                capture new markets and maximize your profits.</p>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4 mt-4 mt-lg-0">
+                            <div class="box" data-aos="fade-up" data-aos-delay="600">
+                                <img src="assets/img/fastest_time_response-02.png" class="img-fluid" alt="" />
+                                <h3>Fastest Response Time</h3>
+                                <p>We pride ourselves on our commitment to help consumer supply chain
+                                augmentation with the fastest turnaround time in the Industry. We
+                                tailor our business to meet your supply side goals and ultimate success.
+                                It is our goal to eliminate all of customers supply chain inefficiencies and downtime.</p>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4 mt-4 mt-lg-0">
+                            <div class="box" data-aos="fade-up" data-aos-delay="400">
+                                <img src="assets/img/Logistic_Excellence-01.jpg" class="img-fluid" alt="" />
+                                <h3>Logistical Excellence</h3>
+                                <p>Whether in rail, truck, or ship, our experienced logistics
+                                team is dedicated to get your product to your door safely and on time.</p>
+                            </div>
+                        </div>
+
+
+                    </div>
+
+                </div>
+
+            </section>
+
+            {/* Values section end */}
+
 
         </div>
 
--- src/ProductCategoryCards.js
+++ src/ProductCategoryCards.js
@@ -8,21 +8,155 @@ import App from './App'
 
 function ProductCategoryCards() {
     return (
-        <Container>
-            <Row>
-                <Col md={3}> <Link to="/silicones"> <App id={0} /> </Link> </Col>
-                <Col md={3}> <Link to="/paints"> <App id={1} /> </Link> </Col>
-                <Col md={3}> <Link to="/silver_ion_technology"> <App id={2} /> </Link> </Col>
-                <Col md={3}> <Link to="/leather_chemicals"> <App id={3} /> </Link> </Col>
-            </Row>
 
-            <Row className="justify-content-center">
-                <Col md={3}> <Link to="/api"> <App id={4} /> </Link> </Col>
-                <Col md={3}> <Link to="/renewable_energy_products"> <App id={5} /> </Link> </Col>
-                <Col md={3}> <Link to="/water_treatment_chemicals"> <App id={6} /> </Link> </Col>
-                <Col md={3}> <Link to="/construction_chemicals"> <App id={7} /> </Link> </Col>
-            </Row>
-        </Container>
+        <div>
+
+            {/* Product section start */}
+            <section id="portfolio" class="portfolio">
+
+                <div class="container" data-aos="fade-up">
+
+                    <header class="section-header">
+                        <h2>Products</h2>
+                        <p>Check our latest products</p>
+                    </header>
+
+                    <div class="row gy-4 portfolio-container" data-aos="fade-up" data-aos-delay="200">
+
+                        <div class="col-lg-12 col-md-12 portfolio-item align-items-stretch">
+                            <Container>
+                                <Row>
+                                    <Col md={3}> <Link to="/silicones"> <App id={0} /> </Link> </Col>
+                                    <Col md={3}> <Link to="/paints"> <App id={1} /> </Link> </Col>
+                                    <Col md={3}> <Link to="/silver_ion_technology"> <App id={2} /> </Link> </Col>
+                                    <Col md={3}> <Link to="/leather_chemicals"> <App id={3} /> </Link> </Col>
+                                </Row>
+
+                                <Row className="justify-content-center">
+                                    <Col md={3}> <Link to="/api"> <App id={4} /> </Link> </Col>
+                                    <Col md={3}> <Link to="/renewable_energy_products"> <App id={5} /> </Link> </Col>
+                                    <Col md={3}> <Link to="/water_treatment_chemicals"> <App id={6} /> </Link> </Col>
+                                    <Col md={3}> <Link to="/construction_chemicals"> <App id={7} /> </Link> </Col>
+                                </Row>
+                            </Container>
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+            {/* Product section end */}
+
+            {/* Team section start */}
+            <section id="team" class="team">
+
+                <div class="container" data-aos="fade-up">
+
+                    <header class="section-header">
+                        <p>Team</p>
+                    </header>
+
+                    <div class="row gy-4 justify-content-center">
+
+                        <div class="col-lg-3 col-md-6 d-flex align-items" data-aos="fade-up" data-aos-delay="400">
+                            <div class="member">
+                                <div class="member-img">
+                                    <img src="assets/img/team/nitin.jpg" class="img-fluid" alt="" />
+                                    <div class="social">
+                                        <a href=""><i class="bi bi-twitter"></i></a>
+                                        <a href=""><i class="bi bi-facebook"></i></a>
+                                        <a href=""><i class="bi bi-instagram"></i></a>
+                                        <a href=""><i class="bi bi-linkedin"></i></a>
+                                    </div>
+                                </div>
+                                <div class="member-info">
+                                    <h4>Nitin Salarpuria</h4>
+                                    <span>Proprieter</span>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </section>
+            {/* Team section end */}
+
+            {/* Contact section start */}
+
+            <section id="contact" className="contact">
+                <div className="container" data-aos="fade-up">
+                    <header className="section-header">
+                        <h2>Contact</h2>
+                        <p>Contact Us</p>
+                    </header>
+                    <div className="row gy-4">
+                        <div className="col-lg-6">
+                            <div className="row gy-4">
+                                <div className="col-md-6">
+                                    <div className="info-box">
+                                        <i className="bi bi-geo-alt" />
+                                        <h3>Address</h3>
+                                        <p>19 Mohini Mohan Road <br />Kolkata, India 700020</p>
+                                    </div>
+                                </div>
+                                <div className="col-md-6">
+                                    <div className="info-box">
+                                        <i className="bi bi-telephone" />
+                                        <h3>Call Us</h3>
+                                        <p>+91 98303 77242 <br />+91 79802 86087 <br /> </p>
+                                    </div>
+                                </div>
+                                <div className="col-md-6">
+                                    <div className="info-box">
+                                        <i className="bi bi-envelope" />
+                                        <h3>Email Us</h3>
+                                        <p>meeraki.chem@gmail.com <br /> <br /> </p>
+                                    </div>
+                                </div>
+                                <div className="col-md-6">
+                                    <div className="info-box">
+                                        <i className="bi bi-clock" />
+                                        <h3>Open Hours</h3>
+                                        <p>Monday - Friday<br />9:00AM - 05:00PM</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-6">
+                            <form action="POST" data-netlify="true">
+                                <div className="row gy-4">
+                                    <div className="col-md-6">
+                                        <input type="text" name="name" className="form-control" placeholder="Your Name" required />
+                                    </div>
+                                    <div className="col-md-6 ">
+                                        <input type="email" className="form-control" name="email" placeholder="Your Email" required />
+                                    </div>
+                                    <div className="col-md-12">
+                                        <input type="text" className="form-control" name="subject" placeholder="Subject" required />
+                                    </div>
+                                    <div className="col-md-12">
+                                        <textarea className="form-control" name="message" rows={6} placeholder="Message" required defaultValue={""} />
+                                    </div>
+                                    <div className="col-md-12">
+                                        <input type="file" name="myfile" id="myfile" placeholder="Upload File" rows={6} />
+                                    </div>
+                                    <div className="col-md-12">
+                                        <div data-netlify-recaptcha="true" rows={6} />
+                                    </div>
+                                    <div className="col-md-12 text-center">
+                                        <button type="submit" name="submit" className="btn btn-success">Send Message</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            {/* Contact section end */}
+
+
+        </div>
     )
 }
 
